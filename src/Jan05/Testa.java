c82100d0c405a46b007425ff00adf3b26a2a71a5
Md. Raisul Islam <raisul778@gmail.com>
2026-01-05 22:18:12 +0600

    hi add

---
+++ src/Jan05/Testa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jan05;
+
+/**
+ *
+ * @author raisulislam
+ */
+public class Testa {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
